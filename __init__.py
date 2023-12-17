588ed9c5a6460156961c0b7de902f82a91f40323
wongjaeno <i.am.notyuxuan22@gmail.com>
2023-12-18 03:16:03 +0800

    general template

--- __init__.py
+++ __init__.py
@@ -3,7 +3,9 @@ import sqlite3
 conn = sqlite3.connect('database.db')
 print("Connected to database successfully")
 
-conn.execute('CREATE TABLE blog (title TEXT, summary TEXT, files FILE, description TEXT)')
+conn.execute("DROP TABLE users")
+#conn.execute('CREATE TABLE user (user_id INTEGER PRIMARY KEY AUTOINCREMENT, username VARCHAR(255) NOT NULL UNIQUE, email VARCHAR(255) NOT NULL UNIQUE, password INT(255) NOT NULL)')
+# conn.execute('CREATE TABLE blog (title TEXT, summary TEXT, files FILE, description TEXT)')
 print("Created table successfully!")
 
 conn.close()
--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-from flask import Flask, render_template, request, redirect, url_for
+import functools
+
+from flask import Flask, render_template, request, redirect, url_for, Blueprint, flash, g, session
+from werkzeug.security import check_password_hash, generate_password_hash
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, logout_user, LoginManager, login_required, logout_user, current_user
 from flask_wtf import FlaskForm
@@ -14,6 +17,35 @@ app.config['SECRET_KEY'] = 'sbufbv8829gf2k'
 db.init_app(app)
 
 
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect('database.db')
+    return db
+
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+
+@app.before_request
+def load_logged_in_user():
+    user_id = session.get('user_id')
+
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = get_db().execute(
+            'SELECT * FROM user WHERE user_id = ?', (user_id,)
+        ).fetchone()
+
+
+bp = Blueprint('auth', __name__, url_prefix='/auth')
+
+
 class Users(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String[20], nullable=False, unique=True)
@@ -21,6 +53,7 @@ class Users(db.Model, UserMixin):
     password = db.Column(db.String[80], nullable=False)
     date_added = db.Column(db.DateTime, default='utf-8')
     password_hash = db.Column(db.String(128))
+
 
 class Signupform(FlaskForm):
     username = StringField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholders": "Username"})
@@ -46,6 +79,11 @@ class BlogForm(FlaskForm):
 
 @app.route('/')
 def home():
+    if 'adminappdev' in session.values():
+        return render_template('admindashboard.html')
+    # elif 'username' in session:
+    #     return f'Welcome, {session["username"]}!'
+
     return render_template('home.html')
 
 
@@ -54,16 +92,73 @@ def aboutus():
     return render_template('aboutus.html')
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    form = LoginForm()
-    return render_template('login.html', form=form)
+    error = None
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['psw']
+        db = get_db()
+
+        user = db.execute(
+            'SELECT * FROM user WHERE username = ?', (username,)
+        ).fetchone()
+        if user is None:
+            error = 'Incorrect Credentials.'
+        elif not check_password_hash(user[3], password):
+            error = 'Incorrect Credentials.'
+        if error is None:
+            session.clear()
+            session['user_id'] = user[0]
+            session['username'] = user[1]
+            session['email'] = user[2]
+            return redirect(url_for('home'))
+
+    return render_template('login.html', error=error)
 
 
-@app.route('/signup')
+@app.route('/signup', methods=['GET', 'POST'])
 def signup():
-    form = Signupform()
-    return render_template('signup.html', form=form)
+    if request.method == 'POST':
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['psw']
+        repeatpsw = request.form['psw-repeat']
+        db = get_db()
+        error = None
+        if not username:
+            error = 'Username is required.'
+        elif not password:
+            error = 'Password is required.'
+        elif password != repeatpsw:
+            error = 'Confirm Password has to be the same.'
+        if error is None:
+            try:
+                db.execute("INSERT INTO user (username, email, password) VALUES (?, ?, ?)", (username, email, generate_password_hash(password)))
+                db.commit()
+            except db.IntegrityError:
+                error = f"User {username} is already registered."
+            else:
+                return redirect(url_for("login"))
+        flash(error)
+    return render_template('signup.html')
+
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('home'))
+
+
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('login'))
+
+        return view(**kwargs)
+
+    return wrapped_view
 
 
 @app.route('/blog')
@@ -81,6 +176,7 @@ def blog():
 
 
 @app.route('/addblog', methods=['GET', 'POST'])
+@login_required
 def addblog():
     if request.method == "POST":
         try:
@@ -102,5 +198,27 @@ def addblog():
         return render_template('addblog.html')
 
 
+@app.route('/shop')
+def shop():
+    return render_template('shop.html')
+
+
+@app.route('/tradein')
+def tradein():
+    return render_template('tradein.html')
+
+
+@app.route('/tradeinform')
+@login_required
+def tradeinform():
+    return render_template('tradeinform.html')
+
+
+@app.route('/eco')
+def eco():
+    return render_template('eco.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
+
