ce8f4e85aceca53d324e91797e862c3ef2d44110
wongjaeno <i.am.notyuxuan22@gmail.com>
2023-12-19 01:07:13 +0800

    fe

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import datetime
 import functools
 
 from flask import Flask, render_template, request, redirect, url_for, Blueprint, flash, g, session
@@ -5,8 +6,8 @@ from werkzeug.security import check_password_hash, generate_password_hash
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, logout_user, LoginManager, login_required, logout_user, current_user
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import InputRequired, Length, ValidationError
+from wtforms import StringField, PasswordField, SubmitField, FileField, TextAreaField
+from wtforms.validators import Length, ValidationError, DataRequired
 import sqlite3
 
 
@@ -55,24 +56,12 @@ class Users(db.Model, UserMixin):
     password_hash = db.Column(db.String(128))
 
 
-class Signupform(FlaskForm):
-    username = StringField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholders": "Username"})
-    password = PasswordField(validators=[InputRequired(), Length(min=4, max=80)], render_kw={"placeholder": "Password"})
-    submit = SubmitField("Sign Up")
-
-    def validate_username(self, username):
-        existing_username = Users.query.filter_by(username=username.data).first()
-        if existing_username:
-            raise ValidationError("That username already exists.")
-
-
-class LoginForm(FlaskForm):
-    username = StringField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholders": "Username"})
-    password = PasswordField(validators=[InputRequired(), Length(min=4, max=80)], render_kw={"placeholder": "Password"})
-    submit = SubmitField("Login")
-
 class BlogForm(FlaskForm):
-    title = StringField("Title", validators=[InputRequired()])
+    title = StringField("Title", validators=[DataRequired()])
+    summary = StringField('Summary', validators=[Length(min=1, max=150), DataRequired()])
+    file = FileField('Attachments', default='')
+    description = TextAreaField('Blog away...', validators=[Length(min=50, max=800), DataRequired()])
+    date_added = datetime.datetime
     submit = SubmitField("Submit")
 
 
