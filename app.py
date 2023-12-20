f834f4c024c2445df18861aa36e6e0c512936b75
wongjaeno <i.am.notyuxuan22@gmail.com>
2023-12-20 22:48:50 +0800

    blog photo

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-import datetime
+import os
+import uuid
+from datetime import date, datetime
 import functools
 
 from flask import Flask, render_template, request, redirect, url_for, Blueprint, flash, g, session
@@ -6,6 +8,7 @@ from werkzeug.security import check_password_hash, generate_password_hash
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, logout_user, LoginManager, login_required, logout_user, current_user
 from flask_wtf import FlaskForm
+from werkzeug.utils import secure_filename
 from wtforms import StringField, PasswordField, SubmitField, FileField, TextAreaField
 from wtforms.validators import Length, ValidationError, DataRequired
 import sqlite3
@@ -15,6 +18,8 @@ app = Flask(__name__)
 db = SQLAlchemy()
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SECRET_KEY'] = 'sbufbv8829gf2k'
+UPLOAD_FOLDER = 'static/img/'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 db.init_app(app)
 
 
@@ -51,19 +56,27 @@ class Users(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String[20], nullable=False, unique=True)
     email = db.Column(db.String[50], nullable=False)
-    password = db.Column(db.String[80], nullable=False)
-    date_added = db.Column(db.DateTime, default='utf-8')
+    date_added = db.Column(db.DateTime, default=datetime.utcnow)
     password_hash = db.Column(db.String(128))
+    profile_pic = db.Column(db.String(), nullable=True)
 
 
 class BlogForm(FlaskForm):
     title = StringField("Title", validators=[DataRequired()])
     summary = StringField('Summary', validators=[Length(min=1, max=150), DataRequired()])
-    file = FileField('Attachments', default='')
+    blog_pic = FileField("Blog Photo")
     description = TextAreaField('Blog away...', validators=[Length(min=50, max=800), DataRequired()])
-    date_added = datetime.datetime
     submit = SubmitField("Submit")
 
+
+class Blog(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(255))
+    title = db.Column(db.String(255))
+    summary = db.Column(db.Text)
+    blog_pic = db.Column(db.String(), nullable=True)
+    description = db.Column(db.Text)
+    date_posted = db.Column(db.DateTime, default=datetime.utcnow)
 
 
 @app.route('/')
@@ -167,25 +180,29 @@ def blog():
 @app.route('/addblog', methods=['GET', 'POST'])
 @login_required
 def addblog():
-    if request.method == "POST":
-        try:
-            username = session['username']
-            title = request.form['title']
-            summary = request.form['summary']
-            files = request.form['files']
-            description = request.form['description']
-            with sqlite3.connect('database.db') as con:
-                cur = con.cursor()
-                cur.execute("INSERT INTO blog (username, title, summary, files, description) VALUES (?,?,?,?,?)", (username, title, summary, files, description))
+    form = BlogForm()
+    poster = session['username']
+    if request.method == 'POST':
+        title = request.form['title']
+        summary = request.form['summary']
+        description = request.form['description']
+        blog_pic = request.files['blog_pic']
+        pic_filename = secure_filename(blog_pic.filename)
+        pic_name = str(uuid.uuid1()) + "_" + pic_filename
+        saver = request.files['blog_pic']
+        saver.save(os.path.join(app.config['UPLOAD_FOLDER'], pic_name))
+        with sqlite3.connect('database.db') as con:
+            cur = con.cursor()
+            cur.execute("INSERT INTO blog (username, title, summary, blog_pic, description) VALUES (?,?,?,?,?)", (poster, title, summary, pic_name, description))
 
-                con.commit()
-        except:
-            con.rollback()
-        finally:
-            con.close()
-            return redirect(url_for('blog'))
+            con.commit()
+
+            print("SUCCESSFUL")
+        con.close()
+        return redirect(url_for('blog'))
+
     else:
-        return render_template('addblog.html')
+        return render_template('addblog.html', form=form)
 
 
 @app.route('/editblog/<int:id>', methods=['GET', 'POST'])
