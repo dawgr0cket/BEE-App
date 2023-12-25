ab7aad35bd752c787a3f1dbb718f80ac64d05513
wongjaeno <i.am.notyuxuan22@gmail.com>
2023-12-26 05:28:40 +0800

    Update tradeinform.py

--- tradeinform.py
+++ tradeinform.py
@@ -1,9 +1,12 @@
+tradein_count = 0
+
+
 class Tradeinform:
-    tradein_count = 0
 
     def __init__(self):
-        Tradeinform.tradein_count += 1
-        self.__trade_id = Tradeinform.tradein_count
+        global tradein_count
+        tradein_count += 1
+        self.__trade_id = tradein_count
 
     def set_tradein_id(self, trade_id):
         self.__trade_id = trade_id
